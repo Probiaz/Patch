bd02bc5f4e0f43a17870fe4e68037ea0b595d055
Alumno <Alumno@PC17>
2019-08-20 09:57:20 -0500

    New

---
+++ CicloFor/CicloFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace kokko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int z, x, y;
+            Console.WriteLine("Imprime los numeros del x hasta el y de -1");
+            Console.WriteLine("Donde arrancamos el ciclo(mayor): ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Donde arrancamos el ciclo(menor): ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto en cuanto: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            for (int i = x; i <= y; i -= z)
+            {
+                Console.WriteLine("i = " + i);
+            }
+        }
+    }
+}
